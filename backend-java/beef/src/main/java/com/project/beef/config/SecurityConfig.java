f510410b963563ef680718b54192ae7ee3e27b88
jin <nsj9905@naver.com>
2025-12-22 13:04:40 +0900

    Update SecurityConfig.java

--- backend-java/beef/src/main/java/com/project/beef/config/SecurityConfig.java
+++ backend-java/beef/src/main/java/com/project/beef/config/SecurityConfig.java
@@ -55,14 +55,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("*")); // 모든 도메인 허용
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+    
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
     }
